f133fc20f2f91ad61c1cac88bd198b1abfa6976a
Ernst Klamer <e.klamer@gmail.com>
2020-08-22 17:28:09 +0200

    v0.3

--- main_utils.py
+++ main_utils.py
@@ -5,16 +5,16 @@
 # Author:      roy.oltmans
 #
 # Created:     23-10-2014
-# Copyright:   (c) roy.oltmans 2014
-# Licence:     <your licence>
+# Copyright:   (c) 2020 Ernst Klamer, roy.oltmans
+# Licence:     MIT license
 #-------------------------------------------------------------------------------
 
-import os, ConfigParser
+import os, configparser
 
 class tools(object):
     #Fetch Configuration
     def fetchConfig(self):
-        Config = ConfigParser.ConfigParser()
+        Config = configparser.ConfigParser()
         ConfigFilePath = os.path.dirname(os.path.abspath(__file__)).replace(' ','\ ') + "/config.ini"
         Config.read(ConfigFilePath)
         return Config
